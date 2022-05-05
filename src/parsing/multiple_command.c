50dcb0d06b0cd3ab8986123a74fd343afe13e14e
swautele <swautle@student.s19.com>
2022-05-05 14:23:07 +0200

    autosave

---
+++ src/parsing/multiple_command.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   multiple_command.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simonwautelet <simonwautelet@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 13:01:11 by simonwautel       #+#    #+#             */
+/*   Updated: 2022/05/05 13:13:33 by simonwautel      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	check_multiple_command(t_param *data, char **envp)
+{
+	char	**commands;
+	int		i;
+
+	commands = ft_split(data->str, ';');
+	i = 0;
+	if (commands)
+	{
+		while (commands[i] != NULL)
+		{
+			data->str = commands[i];
+			just_parse_it(data, envp);
+			i++;
+		}
+	}
+	free_table(commands);
+}
