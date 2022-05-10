497c1e053f27b1ab9b3cea7369bddfa2c5bc5e13
swautele <swautle@student.s19.com>
2022-05-10 14:57:30 +0200

    save sans segfault sur null

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: simonwautelet <simonwautelet@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 12:28:24 by swautele          #+#    #+#             */
-/*   Updated: 2022/05/10 14:17:26 by simonwautel      ###   ########.fr       */
+/*   Updated: 2022/05/10 14:56:51 by simonwautel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,22 @@
 
 void	buccle_readline(t_param *data)
 {
-	if (signal(SIGINT, new_readline) == SIG_ERR)
-		printf("failed to register interrupts with kernel\n");	while (data->str)
+	// if (signal(SIGINT, new_readline) == SIG_ERR)
+	// 	printf("failed to register interrupts with kernel\n");
+	while (data->str)
 	{
 		// printf("readline i sumon you \n");
-		// printf("test\n");
 		data->str = readline(data->prompt);
 		if (data->str && *data->str)
 			add_history (data->str);
 		data->fdin = 0;
 		data->fdout = 1;
-		if (strncmp("exit", data->str, 4) == 0 && (data->str[4] == '\0' || data->str[4] == ' ' || data->str[4] == '\t'))
-			break ;
+		if (data->str)
+		{
+			if (ft_strncmp("exit", data->str, 4) == 0 && (data->str[4] == '\0' || data->str[4] == ' ' || data->str[4] == '\t'))
+				break ;
+		}
+		// printf("test\n");
 		else if (data->str)
 			check_multiple_command(data, data->envp);
 			// just_parse_it(data, envp);
@@ -40,13 +44,14 @@ void	new_readline(int sig)
 	// char	eof;
 
 	// kill(0, SIGSTOP);
+	// printf("received sig %d\n", sig);
 	kill(0, SIGINT);
 	// signal(2, &new_readline);
 	(void)sig;
 	// write(0, "\n", 1);
 	write(1, "\n", 1);
-	if (signal(SIGINT, new_readline) == SIG_ERR)
-		printf("failed to register interrupts with kernel\n");
+	// if (signal(SIGINT, new_readline) == SIG_ERR)
+		// printf("failed to register interrupts with kernel\n");
 	// free (g_data->str);
 	// free (g_data->str);
 	// g_data->str = readline(g_data->prompt);
@@ -66,20 +71,20 @@ void	new_readline(int sig)
 int	main(int argc, char **argv, char **envp)
 {
 	// const char	*prompt = "minishell$>";
-	t_param		*data;
+	// t_param		*data;
 
 	(void)argc;
 	(void)argv;
-	data = malloc(sizeof(t_param));
-	data->envp = envp;
-	g_data = data;
-	data->prompt = "minishell$>";
-	if (signal(SIGINT, new_readline) == SIG_ERR)
-		printf("failed to register interrupts with kernel\n");
+	g_data = malloc(sizeof(t_param));
+	g_data->envp = envp;
+	// g_data = data;
+	g_data->prompt = "minishell$>";
+	// if (signal(SIGINT, new_readline) == SIG_ERR)
+	// 	printf("failed to register interrupts with kernel\n");
 	// data->str = readline(data->prompt);
-	data->str = "";
-	buccle_readline(data);
-	free (data);
+	g_data->str = "";
+	buccle_readline(g_data);
+	free (g_data);
 	write(1, "exit\n", 5);
 	return (0);
 }
