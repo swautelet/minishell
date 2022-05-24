db070666f1aa9f79d8964561bbae8292bfb62b00
swautelet <swautele@student.s19.be>
2022-05-24 18:55:27 +0200

    save debug void

--- Makefile
+++ Makefile
@@ -30,7 +30,7 @@ FILES = $(SRCDIR)minishell.c \
 OBJECT = $(FILES:$(SRCDIR)%.c=$(OBJDIR)%.o)
 HEADER = minishell.h
 NAME = minishell
-CFLAGS = -Werror -Wall -Wextra -Iinclude -g
+CFLAGS = -Werror -Wall -Wextra -Iinclude
 LIB = $(LIBDIR)libft.a
 LIBDIR = libft/
 
--- src/string/cut_beginning_whitespace.c
+++ src/string/cut_beginning_whitespace.c
@@ -6,7 +6,7 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 15:16:43 by swautele          #+#    #+#             */
-/*   Updated: 2022/05/23 15:20:20 by swautele         ###   ########.fr       */
+/*   Updated: 2022/05/24 18:54:58 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	cut_beginning_whitespace(char *str)
 	int	i;
 	int	j;
 
+	if (!str)
+		return ;
 	i = 0;
 	j = 0;
 	while (is_whitespace(str[i]))
