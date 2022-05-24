3905b2512de96454a389297c0b654c8577853f17
swautelet <swautele@student.s19.be>
2022-05-24 16:40:40 +0200

    save pwd 2.0

--- src/built_in/cd.c
+++ src/built_in/cd.c
@@ -6,17 +6,45 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:43:38 by swautele          #+#    #+#             */
-/*   Updated: 2022/05/24 12:31:47 by swautele         ###   ########.fr       */
+/*   Updated: 2022/05/24 16:39:36 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	move_dir(t_param *data)
+static void	new_pwd(t_param *data)
+{
+	t_list	*way;
+	char	*temp;
+
+	way = ft_lstsearch(data->envp, "PWD");
+	free(way->content);
+	temp = where_am_i();
+	way->content = ft_strjoin("PWD=", temp);
+	free(temp);
+}
+
+static void	move_from_here(char **fuck, t_param *data)
 {
 	char	*path;
 	char	*temp;
-	t_list	*way;
+
+	path = where_am_i();
+	temp = ft_strjoin(path, "/");
+	free (path);
+	path = ft_strjoin(temp, fuck[1]);
+	free(temp);
+	if (chdir(path) == 0)
+	{
+		new_pwd(data);
+	}
+	else
+		perror(path);
+	free(path);
+}
+
+void	move_dir(t_param *data)
+{
 	char	**fuck;
 
 	fuck = ft_split(data->str, ' ');
@@ -24,30 +52,17 @@ void	move_dir(t_param *data)
 	{
 		if (chdir(my_getenv(data, "HOME")) == -1)
 			perror(my_getenv(data, "HOME"));
+		else
+			new_pwd(data);
 	}
 	else if (fuck[1][0] == '/')
 	{
 		if (chdir(fuck[1]) == -1)
 			perror(fuck[1]);
+		else
+			new_pwd(data);
 	}
 	else
-	{
-		path = where_am_i();
-		temp = ft_strjoin(path, "/");
-		free (path);
-		path = ft_strjoin(temp, fuck[1]);
-		free(temp);
-		if (chdir(path) == 0)
-		{
-			way = ft_lstsearch(data->envp, "PWD");
-			free(way->content);
-			temp = where_am_i();
-			way->content = ft_strjoin("PWD=", temp);
-			free(temp);
-		}
-		else
-			perror(path);
-		free(path);
-	}
+		move_from_here(fuck, data);
 	free_table(fuck);
 }
