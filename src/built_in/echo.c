f447b591eefab2731adddd07deab79a06354bf50
swautelet <swautele@student.s19.be>
2022-05-24 16:53:34 +0200

    save debug echo de merde

--- src/built_in/echo.c
+++ src/built_in/echo.c
@@ -6,20 +6,44 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 16:58:10 by swautele          #+#    #+#             */
-/*   Updated: 2022/05/24 16:28:16 by swautele         ###   ########.fr       */
+/*   Updated: 2022/05/24 16:52:50 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	echo_loop(int i, char *str, char *copy)
+{
+	char	flag;
+	int		j;
+
+	flag = 0;
+	j = 0;
+	while (str[i])
+	{
+		if ((str[i] == '"' || str[i] == "'"[0]) && flag == 0)
+			flag = str[i];
+		else if (flag == str[i] && flag != 0)
+			flag = 0;
+		else if (flag == 0 && is_whitespace(str[i]) == TRUE)
+		{
+			copy[j++] = str[i];
+			while (is_whitespace(str[i + 1]) == TRUE)
+				i++;
+		}
+		else
+			copy[j++] = str[i];
+		i++;
+	}
+}
+
 void	write_echo(char *str)
 {
 	int		i;
-	char	flag;
-	int		j;
 	char	*copy;
 	char	flag_n;
 
+	printf("i received %s\n", str);
 	flag_n = FALSE;
 	i = 4;
 	while (is_whitespace(str[i]) == TRUE)
@@ -29,40 +53,10 @@ void	write_echo(char *str)
 		i = i + 2;
 		flag_n = TRUE;
 	}
-	flag = 0;
-	j = 0;
 	copy = ft_calloc(sizeof(char), ft_strlen(&str[i]) + 1);
-	while (str[i])
-	{
-		if ((str[i] == '"' || str[i] == "'"[0]) && flag == 0)
-		{
-			flag = str[i];
-			i++;
-		}
-		else if (flag == str[i] && flag != 0)
-		{
-			flag = 0;
-			i++;
-		}
-		else if (flag == 0 && is_whitespace(str[i]) == TRUE)
-		{
-			copy[j] = str[i];
-			j++;
-			i++;
-			while (is_whitespace(str[i]) == TRUE)
-				i++;
-		}
-		else
-		{
-			copy[j] = str[i];
-			j++;
-			i++;
-		}
-	}
+	echo_loop(i, str, copy);
 	printf("%s", copy);
 	free(copy);
 	if (flag_n == FALSE)
-	{
 		printf("\n");
-	}
 }
